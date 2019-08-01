ed70b58ca69bafc908b221f3baa2b00b5679efcb
vitis95 <ivan.devita@thinkopen.it>
2019-08-01 15:34:07 +0200

    L7

--- src/index.js
+++ src/index.js
@@ -233,16 +233,123 @@ class Interruttore extends React.Component {
 
 ReactDOM.render(
     <Interruttore />,
-    document.getElementById('root-5')
+    document.getElementById('root-6')
 );
 
 
+function BenvenutoUtente(props) {
+    return <h1>Bentornato/a!</h1>;
+}
 
+function BenvenutoOspite(props) {
+    return <h1>Autenticati, per favore</h1>;
+}
 
+function Benvenuto(props) {
+    const utenteAutenticato = props.utenteAutenticato;
+    if(utenteAutenticato) {
+        return <BenvenutoUtente />;
+    }
+    return <BenvenutoOspite />;
+}
 
+ReactDOM.render(
+    // Prova a cambiare in utenteAutenticato={true}
+    <Benvenuto utenteAutenticato={false} />,
+    document.getElementById('root-7')
+)
 
+function BottoneLogin(props) {
+    return <button onClick={props.onClick}>Login</button>;
+}
 
+function BottoneLogout(props) {
+    return <button onClick={props.onClick}>Logout</button>;
+}
 
+class ControlloLogin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {utenteAutenticato: false};
+    }
+
+    handleLoginClick(){
+        this.setState({utenteAutenticato: true});
+    }
+
+    handleLogoutClick(){
+        this.setState({utenteAutenticato: false});
+    }
+
+    render() {
+        const utenteAutenticato = this.state.utenteAutenticato;
+        let bottone;
+
+        if(utenteAutenticato) {
+            bottone = (
+                <BottoneLogout onClick={this.handleLogoutClick} />
+            );
+        } else {
+            bottone = (
+                <BottoneLogin onClick={this.handleLoginClick} />
+            );
+        }
+        return (
+            <div>
+                <Benvenuto utenteAutenticato={utenteAutenticato} />
+                {bottone}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ControlloLogin />,
+    document.getElementById('root-7')
+);
+
+function MessaggioAvviso(props) {
+    if (!props.attenzione){
+        return null;
+    }
+
+    return <div className="warning">CIAO</div>;
+}
+
+class Pagina extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {mostraAvviso: true};
+        this.handleToggleClick = this.handleToggleClick.bind(
+            this
+        );
+    }
+    handleToggleClick() {
+        this.setState(state => ({
+            mostraAvviso: !state.mostraAvviso,
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <MessaggioAvviso
+                    attenzione={this.state.mostraAvviso}
+                />
+                <button onClick={this.handleToggleClick}>
+                    {this.state.mostraAvviso ? 'Nascondi' : 'Mostra'}
+                </button>
+            </div>
+        );
+    }
+}
+
+    ReactDOM.render(
+        <Pagina />,
+        document.getElementById('root-8')
+    );
 
 
 

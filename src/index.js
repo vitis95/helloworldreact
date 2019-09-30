f2637123ab60436011c6dae9ad73c5a9b2949cd3
vitis95 <ivan.devita@thinkopen.it>
2019-09-30 11:12:34 +0200

    root-13 FlavorForm

--- src/index.js
+++ src/index.js
@@ -413,8 +413,8 @@ function Blog(props) {
 }
 
 const articoli = [
-    {id: 1, titolo: 'Ciao Mondo', testo: 'Benvenuto in imparando React!'},
-    {id: 2, titolo: 'Installazione', testo: 'Puoi installare React usando npm.'}
+    { id: 1, titolo: 'Ciao Mondo', testo: 'Benvenuto in imparando React!' },
+    { id: 2, titolo: 'Installazione', testo: 'Puoi installare React usando npm.' }
 ];
 ReactDOM.render(
     <Blog articoli={articoli} />,
@@ -425,15 +425,15 @@ ReactDOM.render(
 class NameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = { value: '' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
     handleChange(event) {
         // this.setState({value: event.target.value});
-        this.setState({value: event.target.value.toUpperCase()});
+        this.setState({ value: event.target.value.toUpperCase() });
     }
 
     handleSubmit(event) {
@@ -462,7 +462,7 @@ ReactDOM.render(
 
 
 class EssayForm extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             value: 'Please write an essay about your favorite DOM element.'
@@ -473,7 +473,7 @@ class EssayForm extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({ value: event.target.value });
     }
 
     handleSubmit(event) {
@@ -498,6 +498,53 @@ ReactDOM.render(
     <EssayForm />,
     document.getElementById('root-13')
 );
+
+
+
+
+class FlavorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'coconut' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Pick your favorite flavor:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(
+    <FlavorForm />,
+    document.getElementById('root-14')
+);
+
+
+
 
 
 

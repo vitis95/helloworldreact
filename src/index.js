23b0aa1c62696dc173b13ad540093c7cc79106a9
vitis95 <ivan.devita@thinkopen.it>
2019-09-30 18:24:25 +0200

    root-16 root-17 Calculator e TemperatureInput

--- src/index.js
+++ src/index.js
@@ -598,17 +598,91 @@ ReactDOM.render(
 );
 
 
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil</p>
+}
 
 
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <div><br /><hr /><br />
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
+
+                <BoilingVerdict
+                    celsius={parseFloat (temperature)} />
+            </fieldset>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root-16')
+);
 
 
+const scalesNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
 
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
 
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+    
+    render() {
+        const temperature = this.state.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scalesNames[scale]}:</legend>
+                <input value={temperature} onChange={this.handleChange} />
+            </fieldset>
+        );
+    }
+}
 
+class Calculator2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <TemperatureInput scale='c' />
+                <TemperatureInput scale='f' />
+            </div>
+        )
+    }
+}
 
-
-
-
+ReactDOM.render(
+    <Calculator2 />,
+    document.getElementById('root-17')
+);
 
 
 
